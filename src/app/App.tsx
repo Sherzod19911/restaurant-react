2df473eb08b4cb0a1f7eba2d022b1fd6079ba84d
Sherzod19911 <sherzod.baratov.1991@mail.ru>
2024-02-13 03:32:17 +0900

    feat: develop routing based on restaurant projects

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,6 +6,14 @@ import { RippleBadge } from './MaterialTheme/styled';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Users from "./components/users";
 import Dishes from './components/dishes';
+import { RestaurantPage } from './screens/RestaurantPage';
+import { PagePage } from './screens/PagePage';
+import { ShopPage } from './screens/ShopPage';
+import { BlogPage } from './screens/BlogPage';
+import { HelpPage } from './screens/HelpPage';
+import { ContactusPage,  } from './screens/ContactusPage';
+import { LoginPage } from './screens/LoginPage';
+import { HomePage } from './screens/HomePage';
 
 
 function App() {
@@ -15,25 +23,58 @@ function App() {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+
+
+          <Link to="/restaurant">RestaurantPage</Link>
           </li>
           <li>
-            <Link to="/dishes">Dishes</Link>
+            <Link to="/pages">PagePage</Link>
           </li>
           <li>
-            <Link to="/users">Users</Link>
+            <Link to="/shops">ShopPage</Link>
+          </li>
+          <li>
+            <Link to="/blog">BlogPage</Link>
+          </li>
+          <li>
+            <Link to="/help">HelpPage</Link>
+          </li>
+          <li>
+          <Link to="/Contact us">ContactusPage</Link>
+           </li>
+          <li>
+          <Link to="/login">LoginPage</Link>
+           </li>
+          <li>
+          <Link to="/">Homepage</Link>
+
           </li>
           </ul>
       </nav>
       <Switch>
-        <Route path="/dishes">
-          <Dishes/>
+      <Route path="/restaurant">
+          <RestaurantPage />
         </Route>
-        <Route path="/users">
-          <Users />
+        <Route path="/pages">
+          <PagePage />
+        </Route>
+        <Route path="/shops">
+          <ShopPage />
+        </Route>
+        <Route path="/blog">
+          <BlogPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
+        </Route>
+        <Route path="/Contact us">
+          <ContactusPage />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
         </Route>
         <Route path="/">
-          <Home/>
+          <HomePage/>
         </Route>
       </Switch>
     </div>
