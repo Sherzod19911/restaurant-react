02fb4b51b60ef7251eeef2127433d749b43b6571
Sherzod19911 <sherzod.baratov.1991@mail.ru>
2024-02-13 00:38:43 +0900

    feat:checking react router dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,27 +3,47 @@ import React from 'react';
 import '../css/App.css';
 import { RippleBadge } from './MaterialTheme/styled';
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Users from "./components/users";
+import Dishes from './components/dishes';
+
+
 function App() {
   return (
-    <Container>
-    <Stack flexDirection={"column"}>
-
-      <Box sx={{my: 4}}>
-        <Typography variant= 'h4' component={'h1'} gutterBottom>
-          Create React App on TypeScript with Redux
-        </Typography>
-
-      </Box>
-      
-      <Box>
-        <RippleBadge badgeContent={4}>
-        <Button variant="contained">Contained</Button>
-        </RippleBadge>
-
-      </Box>
-    </Stack>
-  </Container>
-  );
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/dishes">Dishes</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+          </ul>
+      </nav>
+      <Switch>
+        <Route path="/dishes">
+          <Dishes/>
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home/>
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+  )
 }
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
 
 export default App;
